5362a7c6d97fb61ecd76c4ad2960f6659de54355
Lionel <joechipjoechip@gmail.com>
2023-02-08 17:14:44 +0100

    add lot of icons + fine tunning on navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,27 +2,83 @@
 
 
 import { useUserStore } from "@/stores/user"
+import { useGetEventPosition } from "@/composables/getEventPosition";
 
 const store = useUserStore()
+
+const { $on } = useNuxtApp()
+
+$on("main-touch-move", onMainTouchMove)
+
+const decayX = ref(0)
+const decayY = ref(0)
+const transformString = computed(() => `translateX(calc(-50% + ${decayX.value}px)) translateY(${decayY.value}px)`)
+
+function onMainTouchMove( event ){
+
+	const { x, y } = useGetEventPosition(event)
+
+	const cursorIsBottom = (y / window.innerHeight) > 0.8
+	const cursorIsCenter = (x / window.innerWidth) > 0.1 && (x / window.innerWidth) < 0.9
+	const cursorIsNearNavbar = cursorIsBottom && cursorIsCenter
+
+	if( cursorIsNearNavbar ){
+
+		decayX.value = ((x / window.innerWidth) - 0.5 )* 200
+
+		decayY.value = (y / window.innerHeight - 0.8) * -200
+
+	} else {
+
+		decayX.value = 0
+
+		decayY.value = 0
+
+	}
+
+	console.log("decayY", decayY.value)
+
+}
 
 
 </script>
 
 <template>
-	<div class="navbar-wrapper">
+
+	<div 
+		class="navbar-wrapper"
+		:style="{
+			transform: transformString
+		}"
+	>
 
 		<IconsUiChevronLeft 
-			color="#00FF00" 
+			color="#00FF00"
+			title="previous"
 			@click="store.setCurrentStepIndexDecrement()"
 		/>
 
-		<IconsBodyBalance 
-			color="yellow" 
+		<IconsGameIconsCrackedDisc 
+			color="yellow"
+			title="bio"
+			@click="store.setCurrentStepIndex(2)"
+		/>
+
+		<IconsGameIconsDoubleDiaphragm 
+			color="orange"
+			title="formation"
+			@click="store.setCurrentStepIndex(2)"
+		/>
+
+		<IconsGameIconsMoebiusTrefoil
+			color="pink"
+			title="experience"
 			@click="store.setCurrentStepIndex(2)"
 		/>
 
 		<IconsUiChevronRight 
-			color="rebeccapurple" 
+			color="rebeccapurple"
+			title="next"
 			@click="store.setCurrentStepIndexIncrement()"
 		/>
 
@@ -32,37 +88,74 @@ const store = useUserStore()
 
 <style lang="scss" scoped>
 
-	$navbarWidth: 350px;
-
-	$iconWidth: 40px;
+	$iconWidth: 30px;
 	$iconBorderRadius: 18px;
 
 	.navbar {
 		&-wrapper {
 			z-index: 250;
 			position: fixed;
-			bottom: 45px;
-			width: $navbarWidth;
-			padding: 15px;
-			
-			border-radius: $iconBorderRadius;
-			backdrop-filter: blur(5px);
-			box-shadow: 0px 10px 25px rgba(0,0,0,0.2);
-			transform: translateX(calc(50vw - ($navbarWidth / 2)));
-
-			background-color: rgba(0,0,0,0.4);
+			bottom: 25px;
+			left: 50%;
+			transform: translateX(-50%);
 
 			display: flex;
 			justify-content: space-between;
+			
+			padding: 1rem;
+			
+			border-radius: $iconBorderRadius;
+			backdrop-filter: blur(5px);
+			box-shadow: 0px 10px 25px rgba(0,0,0,0.2),
+						inset -1px 1px 1px rgba(255,255,255,0.1);
 
-			.layout-svg-navbar {
+			background-color: rgba(0,0,0,0.4);
+
+			transition: padding .3s,
+						transform .1s;
+
+			&:hover {
+				padding: 2rem;
+
+				:deep(.layout-svg-navbar-wrapper){
+					margin: 0 1.5rem;
+				}
+
+				:deep(.layout-svg-navbar-container) {
+
+					svg {
+						width: 40px;
+						height: 40px;
+					}
+
+				}
+			}
+
+			:deep(.layout-svg-navbar-wrapper) {
+
+				margin: 0 0.75rem;
+
+				transition: margin .3s;
+
+				&:first-of-type {
+					margin-left: 0;
+				}
+				&:last-of-type {
+					margin-right: 0;
+				}
+
+			}
+
+			:deep(.layout-svg-navbar-container) {
 
 				border-radius: $iconBorderRadius;
+				
 
-				:deep(svg) {
+				svg {
 					width: $iconWidth;
 					height: $iconWidth;
 				}
+
 			}
 
 		}
--- layouts/layoutSvgNavbar.vue
+++ layouts/layoutSvgNavbar.vue
@@ -3,48 +3,91 @@ defineProps({
     color: {
         type: String,
         default: "currentColor"
+    },
+    title: {
+        type: String,
+        required: true
     }
 })
 
 </script>
 
 <template>
-    <div class="layout-svg-navbar">
-        <div class="layout-svg-navbar-inner">
-            <slot />
+    <div class="layout-svg-navbar-wrapper">
+
+        <div class="layout-svg-navbar-title">{{ title }}</div>
+
+        <div class="layout-svg-navbar-container">
+
+            <div class="layout-svg-navbar-inner">
+
+                <slot />
+
+            </div>
+
         </div>
+
     </div>
 </template>
 
 <style lang="scss">
 
+    $transitionDuration: 0.3s;
+    $transitionDurationShort: 0.18s;
+
     .layout-svg-navbar {
-        cursor: pointer;
-        overflow: hidden;
-        border-radius: 15px;
-        background-color: rgba(255,255,255,0.09);
-        box-shadow: inset 0 2px 1px rgba(255,255,255,0.1),
-                    inset 0 -2px 1px rgba(0,0,0,0.2);
 
-        transform: translateY(0);
-        transition: box-shadow .1s, 
-                    transform .18s cubic-bezier(0,1.61,1,1.67);
+        &-wrapper {
+            position: relative;
 
-        &:hover {
+            &:hover {
 
-            transform: translateY(-10px);
-            box-shadow: inset -2px 2px 1px rgba(255,255,255,0.1),
-                        inset 1px -1px 1px v-bind(color);
+                .layout-svg-navbar {
 
-            .layout-svg-navbar-inner {
+                    &-container {
+                        transform: translateY(-10px) scale(1.1);
+                        box-shadow: inset -2px 2px 1px rgba(255,255,255,0.1),
+                                    inset 1px -1px 1px v-bind(color),
+                                    0 10px 100px v-bind(color);
+                    }
 
-                &::after {
-                    transform: translateY(-150%);
+                    &-inner {
+
+                        &::after {
+                            width: 100%;
+                            filter: blur(25px);
+                            bottom: -10px;
+                        }
+
+                    }
+
+                    &-title {
+                        opacity: 1;
+                        filter: blur(0);
+                        transform: translateX(-50%) translateY(-180%);
+                        text-shadow: 0 -4px 50px v-bind(color);
+                    }
                 }
 
-            }
+            } 
 
-        } 
+        }
+
+        &-container {
+
+            cursor: pointer;
+            overflow: hidden;
+            border-radius: 15px;
+            background-color: rgba(255,255,255,0.09);
+            box-shadow: inset 0 2px 1px rgba(255,255,255,0.1),
+                        inset 0 -2px 1px rgba(0,0,0,0.2),
+                        1px -1px 1px transparent;
+    
+            transform: translateY(0) scale(1);
+            transition: box-shadow $transitionDurationShort, 
+                        transform $transitionDurationShort cubic-bezier(0,1.61,1,1.67);
+
+        }
 
         &-inner {
             position: relative;
@@ -52,29 +95,46 @@ defineProps({
             flex-direction: column;
             justify-content: center;
             align-items: center;
-            padding: 0.7rem;
+            padding: 1rem;
             overflow: hidden;
+            margin: 0;
 
             svg {
-                filter: drop-shadow(0 0 8px v-bind(color));
+                filter: drop-shadow(0 0 6px v-bind(color));
+
+                transition: width $transitionDuration;
             }
 
             &::after {
                 content: "";
-                width: 30%;
+                width: 20%;
                 height: 10px;
                 background-color: v-bind(color);
                 display: block;
                 position: absolute;
                 bottom: 0;
-                transform: translateY(0);
-                filter: blur(14px);
+                filter: blur(12px);
 
-                transition: transform .5s;
+                transition: all $transitionDuration;
             }
 
         }
 
+        &-title {
+            color:v-bind(color);
+            display: block;
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%) translateY(0);
+
+            text-transform: capitalize;
+            font-size: 1.4rem;
+
+            filter: blur(20px);
+            opacity: 0;
+
+            transition: all $transitionDuration;
+        }
         
     }
 
